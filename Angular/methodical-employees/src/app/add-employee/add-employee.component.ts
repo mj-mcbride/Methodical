847770969c925c4e53147eb88043a33584d40fba
Mary-Jane <mary-janemc@kainos.com>
2017-08-17 11:15:59 +0100

    Added Employee model to add employee view

--- Angular/methodical-employees/src/app/add-employee/add-employee.component.ts
+++ Angular/methodical-employees/src/app/add-employee/add-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'methodical-add-employee',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent implements OnInit {
+  employee: Employee;
 
   constructor() { }
 
