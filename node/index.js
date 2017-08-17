b42634d75c63e79dcbdc34f577003cdf25ca0e81
Dariusz Jerzewski <dariuszjerzewski.dj@gmail.com>
2017-08-17 10:50:31 +0100

    Node link to DB & us2 server-side

--- node/index.js
+++ node/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const db = require('./db.js');
 
-app.get('/employees', function (req, res) {
+app.get('/employees/dept', function (req, res) {
+    console.log('Getting list of employees by dept.');
+    db.getEmployeesInDepartment(req.body.id, function (rows){
+        res.send(rows);
+    });
 });
 
 app.listen(8002, function () {
+    console.log('Listening on 192.168.1.60:8002.');
 });
