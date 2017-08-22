152a527ddb31e8374787c917043396581a9dfd5c
Dariusz Jerzewski <dariuszjerzewski.dj@gmail.com>
2017-08-22 09:31:52 +0100

    Final changes

--- node/index.js
+++ node/index.js
@@ -7,21 +7,21 @@ app.use(bodyParser.json());
 
 app.post('/new_employee', function (req, res) {
     const ename = req.body.forename;
-    console.log('Adding new employee.' + ename);
+    console.log('Adding new employee.\n' + req.body);
     db.addEmployee(req.body, function (rows){
          res.send(rows);
     }); 
 });
 
 app.post('/new_sales_employee', function (req, res) {
-    console.log('Adding new sales employee.' + req.body);
+    console.log('Adding new sales employee.\n' + req.body);
     db.addSalesEmployee(req.body, function (rows){
         res.send(rows);
     });
 });
 
 app.post('/employees/dept', function (req, res) {
-    console.log('Getting list of employees by dept.' + req.body);
+    console.log('Getting list of employees by dept.\n' + req.body);
     db.getEmployeesInDepartment(req.body, function (rows){
         res.send(rows);
     });
